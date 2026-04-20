68ed8804292bb97b4f98bee3d1e603ee0c0cf942
anna-nava98 <anna_nova98@outlook.com>
2026-04-21 00:26:06 +0300

    feat: register all models in Django Admin

---
+++ shop/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Item, Order, Discount, Tax
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'currency')
+    search_fields = ('name',)
+    list_filter = ('currency',)
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stripe_coupon_id', 'percent_off', 'amount_off')
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stripe_tax_rate_id', 'percentage', 'inclusive')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'total_price', 'discount', 'tax', 'created_at')
+    filter_horizontal = ('items',)
