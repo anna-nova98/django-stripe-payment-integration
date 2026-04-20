cbf8f15a6f9b123b841798195215a7bb681b1846
anna-nava98 <anna_nova98@outlook.com>
2026-04-21 01:29:58 +0300

    feat: add create_default_superuser management command

---
+++ shop/management/commands/create_default_superuser.py
@@ -0,0 +1,23 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = 'Create a default superuser from environment variables if none exists'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+
+        if not password:
+            self.stdout.write('DJANGO_SUPERUSER_PASSWORD not set, skipping.')
+            return
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(f'Superuser "{username}" created.')
+        else:
+            self.stdout.write(f'Superuser "{username}" already exists.')
